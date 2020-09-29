e0c52aff2061b71f26ae748f57785fc02deed66f
MaxBrowning <browning.max@gmail.com>
2020-09-28 22:43:16 -0400

    Major progress on PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,13 +7,8 @@ poll_csv = os.path.join("Resources", "02-Homework_03-Python_Instructions_PyPoll_
 
 # set starting lists and values
 candidates = []
-vote_percent = []
-candidate_votes = []
 voters = []
-votes_khan = 0
-votes_correy = 0
-votes_li = 0
-votes_otooley = 0
+vote_tallies = {}
 
 # open the csv
 with open(poll_csv) as csvfile:
@@ -24,43 +19,49 @@ with open(poll_csv) as csvfile:
 
     # convert the candidate column into a list
     for row in csvreader:
+        
+        # add each voter ID to a list
         voters.append(row[0])
             
-        # add up votes for each candidate:
-        if row[2] == "Khan":
-            votes_khan += 1
-        if row[2] == "Correy":
-            votes_correy += 1
-        if row[2] == "Li":
-            votes_li += 1
-        if row[2] == "O'Tooley":
-            votes_otooley += 1
-
-        # append list of candidates with unique candidates
-        if row[2] not in candidates:
+        # add each new candidate to a list and set their votes to 0
+        if row[2] not in vote_tallies:
+            vote_tallies[row[2]] = 0
             candidates.append(row[2])
+
+        # add votes for each candidate
+        vote_tallies[row[2]] += 1
 
 # find the total number of votes (values) in the set
 total_votes = len(voters)
 
-# convert votes to percentage of total
-khan_percent = round((votes_khan / total_votes)*100,3)
-correy_percent = round((votes_correy / total_votes)*100,3)
-li_percent = round((votes_li / total_votes)*100,3)
-otooley_percent = round((votes_otooley / total_votes)*100,3)
+# make list of percents for each candidate
+vote_percents = []
+for value in vote_tallies.values():
+    vote_percents.append(value/total_votes*100)
 
-# print election results
-print("Election Results")
-print("--------------------------")
-print(f"Total Votes: {total_votes}")
-print("--------------------------")
-print(f"Khan: {khan_percent}% ({votes_khan})")
-print(f"Correy: {correy_percent}% ({votes_correy})")
-print(f"Li: {li_percent}% ({votes_li})")
-print(f"O'Tooley: {otooley_percent}% ({votes_otooley})")
-print("--------------------------")
-print("Winner: Khan")
-print("--------------------------")
+# make list of vote totals
+vote_totals = []
+for value in vote_tallies.values():
+    vote_totals.append(value)
+
+for _ in range(len(candidates)):
+    print(f"{candidates[_]}: {vote_percents[_]} ({vote_totals[_]})")
+
+# save election results as output data
+output_data = "Election Results\n"
+output_data += "--------------------------\n"
+output_data += f"Total Votes: {total_votes}\n"
+output_data += "--------------------------\n"
+#output_data += f"Khan: {khan_percent}% ({votes_khan})\n"
+#output_data += f"Correy: {correy_percent}% ({votes_correy})\n"
+#output_data += f"Li: {li_percent}% ({votes_li})\n"
+#output_data += f"O'Tooley: {otooley_percent}% ({votes_otooley})\n"
+output_data += "--------------------------\n"
+output_data += "Winner: Khan\n"
+output_data += "--------------------------\n"
+
+#print output data to terminal
+print(f"{output_data}")
 
 # export text file with results
 # identify export path
@@ -70,17 +71,7 @@ export_path = os.path.join("Analysis", "Election_Results.txt")
 report = open(export_path, 'w')
 
 # add text to .txt document
-report.write("Election Results" + "\n")
-report.write("--------------------------" + "\n")
-report.write(f"Total Votes: {total_votes}" + "\n")
-report.write("--------------------------" + "\n")
-report.write(f"Khan: {khan_percent}% ({votes_khan})" + "\n")
-report.write(f"Correy: {correy_percent}% ({votes_correy})" + "\n")
-report.write(f"Li: {li_percent}% ({votes_li})" + "\n")
-report.write(f"O'Tooley: {otooley_percent}% ({votes_otooley})" + "\n")
-report.write("--------------------------" + "\n")
-report.write("Winner: Khan" + "\n")
-report.write("--------------------------")
+report.write(output_data)
 
 # close file
 report.close
